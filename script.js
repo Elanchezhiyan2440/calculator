e0334acf582d3ce33b7b810459f6e561abd884a2
Elanchezhiyan <elanchezhiyan2440@gmail.com>
2023-07-16 22:54:06 +0530

    BUG FIXES:
    - Bug fixes, a lot of it.
    - New ERROR message instances.
    - if, else and else statements for error messages.
    - Made so that user can't enter more than one decimal point.

--- script.js
+++ script.js
@@ -5,130 +5,108 @@ let operator = "";
 
 const numButtons = [...document.querySelectorAll(`[data-numbers]`)];
 const operatorButtons = [...document.querySelectorAll(`[data-operators]`)];
-const primaryText = document.querySelector("h2");
-const secondaryText = document.querySelector("h3");
+const primaryText = document.querySelector('h2');
+const secondaryText = document.querySelector('h3');
 const clearOneButton = document.querySelector(".clearOneButton");
 const clearAllButton = document.querySelector(".clearAllButton");
 const equalToButton = document.querySelector(".equalToButton");
 
 numButtons.forEach(appendToMainString);
-operatorButtons.forEach(updateOperator);
-clearOneButton.addEventListener("click", clearOne);
-clearAllButton.addEventListener("click", clearAll);
-equalToButton.addEventListener("click", updateSecondOperand);
 
-function appendToMainString(button) {
-  button.addEventListener("click", (event) => {
+operatorButtons.forEach(updateOperator);
+
+clearOneButton.addEventListener('click', clearOne);
+
+clearAllButton.addEventListener('click', clearAll);
+
+equalToButton.addEventListener('click', updateSecondOperand);
+
+function appendToMainString(button){
+    button.addEventListener('click', (event)=>{
     clickedButton = event.target;
     /*The below two lines of code exists so 
     the primaryText will have a placeholder "0" until a number is entered
     and there extra zeroes infront a string is trimmed.
     also stops user from entering multiple zeroes as input*/
-    if (mainString !== "ERROR") {
-      mainString = mainString + clickedButton.innerHTML;
-      mainString = mainString.replace(/^0+/, "");
-      mainString = mainString.toString();
-      updatePrimaryText(mainString);
-    }
-  });
-}
+    if (mainString === "ERROR"){mainString = ""};
+    mainString = mainString + clickedButton.innerHTML;
+    mainString = mainString.replace(/^0+/, '');
+    mainString = mainString.toString()
+    mainString =mainString.split(".", 2).join(".");
+    console.log(mainString)
+    updatePrimaryText(mainString);})
+};
 
-function updateOperator(button) {
-  button.addEventListener("click", (event) => {
-    clickedButton = event.target;
-    operator = clickedButton.innerHTML;
 
-    if (firstOperand !== "" && operator !== "" && secondOperand !== "") {
-      operate(firstOperand, operator, secondOperand);
-      firstOperand = mainString;
-      operator = "";
-      secondOperand = "";
-    } else if (firstOperand !== "" && operator !== "") {
-      secondOperand = mainString;
-      operate(firstOperand, operator, secondOperand);
-      firstOperand = mainString;
-      operator = "";
-      secondOperand = "";
-    } else {
-      firstOperand = mainString;
-    }
-
-    updateSecondaryText(firstOperand, operator);
-    resetMainString();
+function updateOperator(button){
+    button.addEventListener('click', (event)=>{
+        clickedButton = event.target;
+        operator = clickedButton.innerHTML;
+        if (mainString === "ERROR"){mainString = "ERROR"}
+        else if (firstOperand !== "" && operator !== ""){
+            updateSecondOperand();
+            firstOperand = answer;
+            secondOperand = "";
+            operator = "";
+            updatePrimaryText(firstOperand);
+        }
+        else{
+        firstOperand = mainString;
+        updateSecondaryText(firstOperand, operator);
+        resetMainString();
+        updatePrimaryText(mainString);
+        console.log(firstOperand)
+        console.log(operator);}
+    })
+};
+    
+function clearOne(){
+    mainString = mainString.slice(0, -1);
+    console.log(mainString)
     updatePrimaryText(mainString);
-  });
 }
 
-function clearOne() {
-  mainString = mainString.slice(0, -1);
-  updatePrimaryText(mainString);
-}
-
-function clearAll() {
-  operator = "";
-  secondOperand = "";
-  firstOperand = "";
-  resetMainString();
-  updateSecondaryText(firstOperand, operator);
-  updatePrimaryText(mainString);
-  console.table(firstOperand, secondOperand, operator, mainString);
-}
-
-function operate(firstOperand, operator, secondOperand) {
-  let answer = "";
-  if (
-    firstOperand !== "" &&
-    secondOperand !== "" &&
-    operator !== "" &&
-    secondOperand !== "0"
-  ) {
-    switch (operator) {
-      case "+":
-        answer = +firstOperand + +secondOperand;
-        break;
-      case "-":
-        answer = +firstOperand - +secondOperand;
-        break;
-      case "/":
-        answer = +firstOperand / +secondOperand;
-        break;
-      case "%":
-        answer = +firstOperand % +secondOperand;
-        break;
-      case "*":
-        answer = +firstOperand * +secondOperand;
-        break;
-    }
-    mainString = answer;
-    firstOperand = answer;
-    secondOperand = "";
+function clearAll(){
     operator = "";
-    updatePrimaryText(mainString);
-    updateSecondaryText(firstOperand, operator);
-  } else answer = "ERROR";
-
-  if (answer === Infinity || answer === NaN) {
-    answer = "ERROR";
+    secondOperand = "";
     firstOperand = "";
-    secondOperand = "";
-    operator = "";
+    resetMainString();
+    updateSecondaryText(firstOperand, operator);
     updatePrimaryText(mainString);
-  }
-  mainString = answer;
-  firstOperand = "";
-  secondOperand = "";
-  operator = "";
-  updatePrimaryText(mainString);
-  updateSecondaryText(firstOperand, operator);
+    console.table(firstOperand, secondOperand, operator, mainString);
 }
 
-function updateSecondOperand() {
-  if (mainString !== "ERROR") {
-    secondOperand = mainString;
-    operate(firstOperand, operator, secondOperand);
-  }
-}
 
-let updateSecondaryText = (firstOperand, operator) =>(secondaryText.textContent = firstOperand + operator);
-let updatePrimaryText = (str) => (primaryText.textContent = str);
-let resetMainString = () => (mainString = "0");
+function operate(firstOperand, operator, secondOperand){
+    let answer = "";
+    if(firstOperand !== "" && secondOperand !== "" && operator !== "" && secondOperand !== ""){
+        switch(operator){
+            case "+": answer = +firstOperand + +secondOperand;
+            break;
+            case "-": answer = +firstOperand - +secondOperand;
+            break;
+            case "/": answer = +firstOperand / +secondOperand;
+            break;
+            case "%": answer = +firstOperand % +secondOperand;
+            break;
+            case "*": answer = +firstOperand * +secondOperand;
+            break;
+        }
+    } else {answer = "ERROR"};
+    if(answer === Infinity){answer = "ERROR";}
+    if(answer === NaN){answer = "ERROR";}
+    clearAll();   
+    mainString = answer;
+    updatePrimaryText(mainString);
+};
+
+function updateSecondOperand(){
+    if(mainString !== "ERROR"){
+        secondOperand = mainString;
+        operate(firstOperand, operator, secondOperand);
+        console.log(secondOperand);}
+};
+
+let updateSecondaryText = (firstOperand, operator) => secondaryText.textContent = firstOperand + operator;
+let updatePrimaryText = (str) => primaryText.textContent = str; 
+let resetMainString = () => mainString = "0";
