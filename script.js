75526cd7903c54b7abe10f9053383903893b8fe8
Elanchezhiyan <elanchezhiyan2440@gmail.com>
2023-07-16 22:59:26 +0530

    CLEAN UP AT AISLE 7:
    - Cleaned up code.
    - Minor changes with comments.
    - removed console.log messages.

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
+//Variables:
+
 let mainString = "0";
 let firstOperand = "";
 let secondOperand = "";
 let operator = "";
+
+//DOM variables:
 
 const numButtons = [...document.querySelectorAll(`[data-numbers]`)];
 const operatorButtons = [...document.querySelectorAll(`[data-operators]`)];
@@ -11,32 +15,30 @@ const clearOneButton = document.querySelector(".clearOneButton");
 const clearAllButton = document.querySelector(".clearAllButton");
 const equalToButton = document.querySelector(".equalToButton");
 
+//Functions attached to DOM variables:
+
 numButtons.forEach(appendToMainString);
-
 operatorButtons.forEach(updateOperator);
-
 clearOneButton.addEventListener('click', clearOne);
-
 clearAllButton.addEventListener('click', clearAll);
-
 equalToButton.addEventListener('click', updateSecondOperand);
+
+//Functions:
 
 function appendToMainString(button){
     button.addEventListener('click', (event)=>{
     clickedButton = event.target;
     /*The below two lines of code exists so 
     the primaryText will have a placeholder "0" until a number is entered
-    and there extra zeroes infront a string is trimmed.
+    and the extra zeroes infront a string is trimmed.
     also stops user from entering multiple zeroes as input*/
     if (mainString === "ERROR"){mainString = ""};
     mainString = mainString + clickedButton.innerHTML;
     mainString = mainString.replace(/^0+/, '');
     mainString = mainString.toString()
-    mainString =mainString.split(".", 2).join(".");
-    console.log(mainString)
+    mainString =mainString.split(".", 2).join("."); //To stop user from entering more than 1 decimal point.
     updatePrimaryText(mainString);})
 };
-
 
 function updateOperator(button){
     button.addEventListener('click', (event)=>{
@@ -54,15 +56,12 @@ function updateOperator(button){
         firstOperand = mainString;
         updateSecondaryText(firstOperand, operator);
         resetMainString();
-        updatePrimaryText(mainString);
-        console.log(firstOperand)
-        console.log(operator);}
+        updatePrimaryText(mainString);}
     })
 };
     
 function clearOne(){
     mainString = mainString.slice(0, -1);
-    console.log(mainString)
     updatePrimaryText(mainString);
 }
 
@@ -103,8 +102,7 @@ function operate(firstOperand, operator, secondOperand){
 function updateSecondOperand(){
     if(mainString !== "ERROR"){
         secondOperand = mainString;
-        operate(firstOperand, operator, secondOperand);
-        console.log(secondOperand);}
+        operate(firstOperand, operator, secondOperand);}
 };
 
 let updateSecondaryText = (firstOperand, operator) => secondaryText.textContent = firstOperand + operator;
